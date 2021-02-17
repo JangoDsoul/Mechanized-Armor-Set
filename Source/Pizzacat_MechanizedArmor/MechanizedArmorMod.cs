f2022e5fbca2b123fbbb69896d0bd0a5fef2ff92
NathanHarbour <nathanharbour@gmail.com>
2021-02-17 17:00:53 -0600

    Adding code for settings to change SteamPrototypes tech level

---
+++ Source/Pizzacat_MechanizedArmor/MechanizedArmorMod.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace Pizzacat_MechanizedArmor
+{
+    public class MechanizedArmorMod : Mod
+    {
+        private static MechanizedArmorMod _instance;
+        public static MechanizedArmorMod Instance => _instance;
+
+        public static MechanizedArmorModSettings settings;
+
+        public MechanizedArmorMod(ModContentPack content) : base(content)
+        {
+            _instance = this;
+            settings = GetSettings<MechanizedArmorModSettings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Mechanized Armor";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            settings.UpdateSettings();
+        }
+    }
+}
